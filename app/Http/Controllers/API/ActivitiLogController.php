5ddb0d8205293bbcdd5d58da5157dbf2609b8e3f
truongshuan <xuanptpc04031@fpt.edu.vn>
2023-11-17 15:52:51 +0700

    [Update] - Fix API Nhật ký hoạt động theo shop

--- app/Http/Controllers/API/ActivitiLogController.php
+++ app/Http/Controllers/API/ActivitiLogController.php
@@ -4,30 +4,45 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Exception;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Activitylog\Models\Activity;
 
 class ActivitiLogController extends Controller
 {
-    public function getActivities()
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
+     * Get all activities
+     *
+     * @return JsonResponse
+     */
+    public function getActivities(): JsonResponse
     {
         try {
-            $data = [];
+            $shop_id = Auth::user()->shop->id;
 
-            $activities = Activity::query()->orderByRaw('created_at DESC')
+            $activities = Activity::query()
+                ->where('event', $shop_id)
+                ->orderByDesc('created_at')
                 ->get();
-            foreach ($activities as $activity) {
-                $data[] = [
-                    'id' => $activity->id,
-                    'author' => $activity->causer ? $activity->causer->full_name : null,
+
+            $data = $activities->map(function ($activity) {
+                return [
+                    'id'          => $activity->id,
+                    'author'      => $activity->causer ? $activity->causer->full_name : 'Anonymous',
+                    'shop'        => $activity->event,
                     'action_type' => $activity->log_name,
-                    'action' => $activity->description,
-                    'content' => $activity->properties['content'],
-                    'action_date' => date('d-m-Y', strtotime($activity->created_at)),
-                    'ip' => $activity->properties['ip'],
-                    'userAgent' => $activity->properties['user_agent'],
+                    'action'      => $activity->description,
+                    'content'     => $activity->properties['content'],
+                    'action_date' => $activity->created_at->format('d-m-Y'),
+                    'ip'          => $activity->properties['ip'],
+                    'userAgent'   => $activity->properties['user_agent'],
                 ];
-            }
+            })->toArray();
 
             return jsonResponse($data, 200, 'Get all activities successfully');
         } catch (Exception $e) {
--- app/Http/Helpers/helper.php
+++ app/Http/Helpers/helper.php
@@ -1,5 +1,9 @@
 <?php
 
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 if (!function_exists('roundToTwoSignificantDigits')) {
     /**
      * Làm tròn số về 2 chữ số đầu tiên
@@ -10,9 +14,39 @@ if (!function_exists('roundToTwoSignificantDigits')) {
      */
     function roundToTwoSignificantDigits($number): float|int
     {
-        if($number == 0) return 0;
+        if ($number == 0) return 0;
         $powerOfTen = pow(10, floor(log10($number)) - 1);
 
         return round($number / $powerOfTen) * $powerOfTen;
+    }
+}
+
+
+if (!function_exists('logActivity')) {
+
+    /**
+     * Helper logging activity
+     *
+     * @param string $log_name
+     * @param mixed  $request
+     * @param string $content
+     * @param string $action
+     *
+     * @return void
+     */
+    function logActivity(string $log_name, mixed $request, string $content, string $action): void
+    {
+        $user = Auth::user();
+        if ($user->type === User::ADMIN_SHOP) {
+            activity($log_name)
+                ->causedBy(Auth::user())
+                ->event(Auth::user()->shop->id)
+                ->withProperties([
+                    'ip'         => $request->getClientIp(),
+                    'user_agent' => $request->header('User-Agent'),
+                    'content'    => $content,
+                ])
+                ->log($action);
+        }
     }
 }
